abe8220990d1d988ce79788564761d297d58a6ba
puneet2121 <bedipuneet21@gmail.com>
2022-09-08 17:17:44 -0600

    add schema files

---
+++ backend/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+ name: {
+  type: String,
+  required: true,
+  maxlength: 32,
+  trim:true
+ },
+ lastname: {
+  type: String,
+  required: false,
+  maxlength: 32,
+  trim:true
+ },
+ email: {
+  type: String,
+  required: true,
+  trim: true,
+  unique: true
+ },
+ password: {
+  type: String,
+  trim: true
+ },
+ salt: String,
+
+ role: {
+  type: Number,
+  default: 0,
+ },
+ purchases: {
+  type: Array,
+  default: []
+ }
+
+});
