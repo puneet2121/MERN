c2cbe7353376558a7e7c20fee8fc125b73c3f53c
puneet2121 <bedipuneet21@gmail.com>
2022-09-10 17:32:12 -0600

    make routes

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,14 @@ require('dotenv').config()
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const authRoutes = require('./routes/auth');
 
+//Middlewares
 app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(cors());
 
+//Database
 mongoose.connect(process.env.DATABASE, 
 {useNewUrlParser: true,
  useUnifiedTopology: true,
@@ -18,9 +21,13 @@ mongoose.connect(process.env.DATABASE,
   console.log('We are connected to DB')
 })
 
+//Routes
+app.use('/api',authRoutes);
 
+//PORT
 const port = process.env.PORT || 8000;
 
+// Server
 app.listen(port,() => {
   console.log(`App is running at ${port} `)
 })
