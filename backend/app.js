006652574b0439bf999c11c639ad3af16cdf328e
Puneet kumar <73667955+puneet2121@users.noreply.github.com>
2022-09-09 18:01:10 -0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/MyStore', 
+mongoose.connect('mongodb://localhost:', 
 {useNewUrlParser: true,
  useUnifiedTopology: true,
  useCreateIndex: true
