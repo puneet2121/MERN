91cf3a3703f9e6fdfdf598ce8e5b3548ca341d4c
puneet2121 <bedipuneet21@gmail.com>
2022-09-12 01:50:30 -0600

    use form data

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require('../models/product')
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
 
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
@@ -11,5 +14,41 @@ exports.getProductById = (req, res, next, id) => {
     }
     req.product = product;
     next();
+  })
+}
+
+exports.createProduct = (req,res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  from.parse(req,(err, fields, file) => {
+    if(err) {
+      return res.status(400).json({
+        error: "Problems with Image"
+      });
+    }
+    //destructure the fields
+    const {name, description} = fields;
+    let product = new Product(fields)
+
+    //handle the file
+    if(file.photo) {
+      if(file.photo.size > 3000000) {
+        return res.status(400).json({
+          error:'file size is too big'
+        })
+      }
+      product.photo.data = fs.readFileSync(file.photo.path)
+      product.photo.contentType = file.photo.type
+    }
+    // save to DB
+    product.save((err,product) => {
+      if(err) {
+        res.status(400).json({
+          error: 'Saving product in DB failed'
+        })
+      }
+      res.json(product);
+    })
   })
 }
