6296f4590485c33786389c33b469841a2a230c53
puneet2121 <bedipuneet21@gmail.com>
2022-09-19 23:58:11 -0600

    add base file

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../styles.css'
 import { API } from '../backend'
+import Base from './Base'
 export default function Home() {
   console.log('api is',API);
   return (
     <div>
+      <Base>
       <h1 className="text-white">hello </h1>
+      </Base>
     </div>
   )
 }
