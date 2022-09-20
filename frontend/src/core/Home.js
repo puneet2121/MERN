8e3e1ab5a1ea41621c022d45d49c6313f704d0cc
puneet2121 <bedipuneet21@gmail.com>
2022-09-19 22:15:29 -0600

    connect backend to frontend

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../styles.css'
+import { API } from '../backend'
 export default function Home() {
+  console.log('api is',API);
   return (
     <div>
       <h1 className="text-white">hello </h1>
