039847eed2e5e4dbcdbb45cc7abaea66c17be30b
puneet2121 <bedipuneet21@gmail.com>
2022-09-21 01:00:58 -0600

    set up the signin route

--- frontend/src/AllRoutes.js
+++ frontend/src/AllRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './core/Home';
+import Signin from './user/Signin';
 import Signup from './user/Signup';
 function AllRoutes() {
   return (
@@ -8,6 +9,7 @@ function AllRoutes() {
       <Routes>
         <Route path='/' exact element= { <Home />} ></Route>
         <Route path='/signup' exact element = { <Signup />}></Route>
+        <Route path='/signin' exact element = { <Signin />}></Route>
       </Routes>
     </BrowserRouter>
   )
