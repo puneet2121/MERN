60ae912977aca6a75af326ac242f0259f3097f29
puneet2121 <bedipuneet21@gmail.com>
2022-09-21 00:57:35 -0600

    set up the signup route

--- frontend/src/AllRoutes.js
+++ frontend/src/AllRoutes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './core/Home';
+import Signup from './user/Signup';
 function AllRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' exact element= { <Home />} ></Route>
+        <Route path='/signup' exact element = { <Signup />}></Route>
       </Routes>
     </BrowserRouter>
   )
